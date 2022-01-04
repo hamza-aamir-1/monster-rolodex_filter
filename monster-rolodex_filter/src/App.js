f426afe987a5afe884d04afd9b940397321a1f03
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-01-04 23:05:12 +0500

    changing into Class Component from Function

--- monster-rolodex_filter/src/App.js
+++ monster-rolodex_filter/src/App.js
@@ -1,9 +1,11 @@
+import React, {Component} from 'react';
 import logo from './monster.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>
@@ -14,7 +16,8 @@ function App() {
         </h4>
       </header>
     </div>
-  );
+    );
+  };
 }
 
 export default App;
