bfd288c6a23a2cbb4655110f19ef95780e231de9
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-01-06 23:46:06 +0500

    Cards Component Created

--- monster-rolodex_filter/src/App.js
+++ monster-rolodex_filter/src/App.js
@@ -18,14 +18,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <CardList>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id} className='monster-heading'>{monster.name}</h1>
-        ))}
-        {this.state.monsters.map(monster => (
-          <h4 key={monster.id}>{monster.email}</h4>
-        ))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   };
--- monster-rolodex_filter/src/components/card-list/card-list.jsx
+++ monster-rolodex_filter/src/components/card-list/card-list.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './card-list.css';
+import {Card} from '../card/card.jsx';
 
-export const CardList = (props) => {
-    return(
-        <div className='card-list'>{props.children}</div>
-    );
-}
+export const CardList = props => (
+        <div className='card-list'>
+            {props.monsters.map(monster => (
+                <Card key={monster.id} monster={monster} />
+            ))}    
+        </div>
+)
