7ace9e4ed190e323020aa73e77248bb56fce2152
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-01-09 21:19:52 +0500

    Update search-box.jsx

--- monster-rolodex_filter/src/components/search-box/search-box.jsx
+++ monster-rolodex_filter/src/components/search-box/search-box.jsx
@@ -3,6 +3,7 @@ import './search-box.css';
 
 export const SearchBox = ({placeholder, handleChange}) => (
     <input 
+        className='search'
         type='search' 
         placeholder={placeholder} 
         onChange={handleChange} 
